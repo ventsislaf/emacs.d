d535661a2dc6107cdc067a26db8b343d66b66266
Ventsislav Nikolov <vencislaf@gmail.com>
2015-07-14 14:55:10 +0100

    Use ujelly theme

--- parts/display-prt.el
+++ parts/display-prt.el
@@ -2,4 +2,8 @@
 (tool-bar-mode -1) ; No toolbar
 (scroll-bar-mode -1) ; No scrollbar
 
+;; Use ujelly theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'ujelly t)
+
 (provide 'display-prt)
