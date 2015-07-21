1a38cf76cd652c47a0f9cee6bb9a12ef1e103e65
Ventsislav Nikolov <vencislaf@gmail.com>
2015-07-21 16:29:16 +0100

    No lock, backup, auto-save files

--- parts/system-prt.el
+++ parts/system-prt.el
@@ -3,4 +3,8 @@
 (setq save-place-file "~/.emacs.d/saveplace")
 (setq-default save-place t)
 
+(setq create-lockfiles nil) ; Don't lock files. It avoids creating .#files
+(setq make-backup-files nil) ; Don't create backup files
+(setq auto-save-default nil) ; Don't autosave
+
 (provide 'system-prt)
